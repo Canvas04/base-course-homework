7b926c71d781ae30781f9eeaf67b808d800acc01
ruslanio_nwa <51386828+Canvas04@users.noreply.github.com>
2025-06-01 12:16:03 +0300

    Update Election.sol

--- Election.sol
+++ Election.sol
@@ -6,7 +6,7 @@ contract Election {
 
     uint256 public maxVotes;
     uint256 public allVotes;
-    uint256 public electionEndTimeInMinutes;
+    uint256 public electionEndTime;
     address public owner;
     bool public isElectionActive;
 
@@ -14,12 +14,12 @@ contract Election {
 
     mapping(uint256 => uint256) public numberOfVotes;
 
-    constructor(string[] memory _electors, uint256 _maxVotes,uint256 _electionEndTimeInMinutes) {
+    constructor(string[] memory _electors, uint256 _maxVotes,uint256 _electionEndTime) {
         electors = _electors;
         maxVotes = _maxVotes;
         owner = msg.sender;
         isElectionActive = true;
-        electionEndTimeInMinutes = _electionEndTimeInMinutes * 60 + block.timestamp;
+        electionEndTime = _electionEndTime + block.timestamp;
     }
 
     modifier onlyOwner() {
@@ -33,14 +33,20 @@ contract Election {
         require(maxVotes > allVotes, "Maximum number of votes exceeded");
         require(owner != msg.sender, "Owner doesn`t vote");
         require(isElectionActive == true, 'Elections are closed');
-        require(block.timestamp < electionEndTimeInMinutes, 'Time of elections is ended');
+        require(block.timestamp < electionEndTime, 'Time of elections is ended');
         userVotes[msg.sender] = true;
         numberOfVotes[_number] += 1;
         allVotes += 1;
     }
 
     function stopVote() public onlyOwner {
-        isElectionActive = false;
+        electionEndTime = block.timestamp;
+    }
+
+    function resetMaxVotes(uint256 _newMaxVotes) public onlyOwner {
+         require(_newMaxVotes > maxVotes,"You can`t decrease maxVotes");
+         maxVotes = _newMaxVotes;
+      
     }
 }
 
